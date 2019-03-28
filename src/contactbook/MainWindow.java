4418a239aa207e336ab68868c0e71b68d5ad8ef2
BEN <benoitmazzon@gmail.com>
2019-03-28 15:29:10 +0100

    structure des classes

---
+++ src/contactbook/MainWindow.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package contactbook;
+
+/**
+ *
+ * @author BEN Formation
+ */
+public class MainWindow implements AdressBook {
+    
+    /**
+     *
+     */
+    @Override
+    public void Afficher();
+}
